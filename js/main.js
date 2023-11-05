9d9b12dd0da657c3a76233e8431a7854a58dee47
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2023-11-05 21:05:11 +0200

    Fixed the Java Script code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const toggler = document.querySelector(".nav__toggler");
-const navbar = document.querySelector(".navbar");
+const navbar = document.querySelector(".nav");
 toggler.addEventListener("click", (e) => {
-  navbar.classList.toggle("expanded");
+  navbar.classList.toggle("nav__expanded");
 });
