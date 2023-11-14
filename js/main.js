3b91be12715ebc2279f68725c8b91eb23f49856f
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2023-11-14 17:54:54 +0200

    Put all components together.
    A few bugs need to be fixed.

--- js/main.js
+++ js/main.js
@@ -20,11 +20,11 @@ tabs.forEach((tab) => {
   });
 });
 
-const accordionHeaders = document.querySelectorAll(".accordion__header");
-const accordion = document.querySelector(".accordion");
-accordionHeaders.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    console.log(e.target.parentElement);
-    e.target.parentElement.classList.toggle("accordion__expanded");
-  });
-});
+// const accordionHeaders = document.querySelectorAll(".accordion__header");
+// const accordion = document.querySelector(".accordion");
+// accordionHeaders.forEach((item) => {
+//   item.addEventListener("click", (e) => {
+//     console.log(e.target.parentElement);
+//     e.target.parentElement.classList.toggle("accordion__expanded");
+//   });
+// });
